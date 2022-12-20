70b7bdac17bd6e54ea846d34f35984f88bce004f
Will Vining <awvining91@gmail.com>
2022-12-19 21:01:30 -0700

    updated webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,87 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+
+      header: './src/js/header.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+
+      new HtmlWebpackPlugin({
+
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+
+
+
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+
+        description: 'Just another text editor',
+        background_color: '#225ca2',
+
+        theme_color: '#225ca2',
+
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
     ],
 
+
+      
+   
+
     module: {
+
+      // These are the loaders 
+
       rules: [
-        
+        {
+          
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          
+          exclude: /node_modules/,
+          // This is the babel loader
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
