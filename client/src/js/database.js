2ff6a17f859aa0a22308252391820bc3494f00ff
Will Vining <awvining91@gmail.com>
2022-12-19 21:53:42 -0700

    newest updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,3 +19,6 @@ export const putDb = async (content) => console.error('putDb not implemented');
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
+
+//I am still working on this section......
+//Happy holidays!!!! :) :)
